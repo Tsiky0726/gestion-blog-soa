09e7412bebdef94668fa098751f2a4c36487f79d
Tsiky <ramelsonandriani@gmail.com>
2024-05-22 22:49:28 +0300

    correction res  delete article

--- controllers/articleController.js
+++ controllers/articleController.js
@@ -66,7 +66,7 @@ exports.deleteArticle = async (req, res) => {
         where: { IdArticle: IdArticle }
       });
       if (deleted) {
-        res.status(204).json();
+        res.status(200).json(deleted);
       } else {
         res.status(404).json({ error: 'Article not found' });
       }
